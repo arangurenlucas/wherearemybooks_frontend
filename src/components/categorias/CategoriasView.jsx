11d87915c92ef6433ecb3adf62228a3af7d1c686
Lucas Aranguren <arangurenlucas@live.com>
2021-05-08 02:47:43 -0300

    Vista categoria con listado de libros relacionados

--- src/components/categorias/CategoriasView.jsx
+++ src/components/categorias/CategoriasView.jsx
@@ -1,21 +1,45 @@
 import React from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams, useHistory } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import swal from "sweetalert";
 
 export default function CategoriasView(props) {
-  const dispatch = useDispatch();
   const params = useParams();
-
-  const [list, setList] = React.useState([]);
+  const [categorias, setCategorias] = React.useState("");
+  const [libros, setLibros] = React.useState("");
   const [error, setError] = React.useState("");
 
+  const getOverHere = async () => {
+    try {
+      const respuestaLibros = await axios.get(
+        "http://localhost:3001/api/libros"
+      );
+      const listaLibros = respuestaLibros.data;
+      setLibros(
+        listaLibros.filter((unLibro) => unLibro.categoria_id == params.id)
+      );
+
+      const respuestaCategorias = await axios.get(
+        "http://localhost:3001/api/categorias/" + params.id
+      );
+      setCategorias(respuestaCategorias.data);
+    } catch (e) {
+      swal("Error", e.response.data, "error");
+    }
+  };
+
+  React.useEffect(async () => {
+    getOverHere();
+  }, []);
   return (
     <>
-      <h2>sad</h2>
+      <h2>{categorias.nombre}</h2>
       <Link to="/categorias">Volver al listado de categorias</Link>
-
-      <ul>sad</ul>
+      <ul>
+        {libros
+          ? libros.map((unLibro) => <li key={unLibro.id}>{unLibro.nombre}</li>)
+          : null}
+      </ul>
     </>
   );
 }
